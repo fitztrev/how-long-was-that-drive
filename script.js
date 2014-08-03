e28c234a43a44378c121aab671ea427490356216
Trevor Fitzgerald <fitztrev@gmail.com>
2014-08-03 13:26:28 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+var from_loc;
+var to_loc;
+
+$('.set_location').click(function(e){
+	e.preventDefault();
+
+	$('#loading').show();
+	$(this).hide();
+
+	navigator.geolocation.getCurrentPosition(save_location, handle_error, {
+		enableHighAccuracy: true
+	});
+});
+
+function save_location(position) {
+	if ( from_loc ) {
+		to_loc = position.coords;
+		calculate_distance();
+	} else {
+		from_loc = position.coords;
+		$('#to_loc').show();
+	}
+	$('#loading').hide();
+}
+
+function handle_error(err) {
+	if (err.code == 1) {
+		alert('Location permission needed.');
+	} else {
+		alert('Network error: Can\'t find position.');
+	}
+}
+
+function calculate_distance() {
+	var meters = getDistance(from_loc.latitude, from_loc.longitude,
+	                                  to_loc.latitude, to_loc.longitude);
+	var yards = meters * 1.09361;
+	$('#results').html(yards + ' yards (' + meters + ' meters)');
+}
